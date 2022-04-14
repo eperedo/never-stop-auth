fed9fd1f2d11dedda2606a9958655193cad623ac
Eduardo P. Rivero <jrperedo@gmail.com>
2022-04-14 12:48:58 -0500

    feat: using chrome lambda instead full puppeteer

--- src/platzi/student.js
+++ src/platzi/student.js
@@ -1,12 +1,21 @@
-const puppeteer = require('puppeteer');
+// const puppeteer = require('puppeteer');
+const chromium = require('chrome-aws-lambda');
+const puppeteer = require('puppeteer-core');
+const UA = require('user-agents');
 
 async function getStudentInformation(username) {
+	// const browser = await puppeteer.launch({
+	// 	headless: process.env.P_HEADLESS || false,
+	// 	args: ['--start-maximized'],
+	// });
 	const browser = await puppeteer.launch({
-		headless: process.env.P_HEADLESS || false,
-		args: ['--start-maximized'],
+		args: chromium.args,
+		executablePath: process.env.P_PATH || (await chromium.executablePath),
+		headless: true,
 	});
 	const page = await browser.newPage();
-
+	let userAgent = new UA();
+	await page.setUserAgent(userAgent.toString());
 	await page.goto(`https://platzi.com/p/${username}`);
 
 	page.on('console', (consoleObj) => console.log(consoleObj.text()));
